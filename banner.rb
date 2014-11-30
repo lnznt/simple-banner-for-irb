d5cdaee38b5858d2489eff13ed944cb1622d969e
lnznt <lnznt0+github@gmail.com>
2014-11-30 11:48:06 +0900

    Update banner.rb

--- banner.rb
+++ banner.rb
@@ -2,45 +2,44 @@
 
 instance_eval do
   #
-  # version color
+  # configuration
   #
-  version_color = RUBY_VERSION >= '2.1' ? :RED     :
+  version_color = RUBY_VERSION >= '2.3' ? :YELLOW  :
+                  RUBY_VERSION >= '2.2' ? :CYAN    :
+                  RUBY_VERSION >= '2.1' ? :RED     :
                   RUBY_VERSION >= '2.0' ? :BLUE    :
                   RUBY_VERSION >= '1.9' ? :MAGENTA :
-                  RUBY_VERSION >= '1.8' ? :GREEN   : :YELLOW
+                  RUBY_VERSION >= '1.8' ? :GREEN   : false
+
+  banner_type = :DEFAULT
 
   #
-  # escape sequence
+  # procedure
   #
-  e = Hash.new {|h,k| h[k] = k.to_s =~ /\A[[:lower:]]/ ? proc {""} : "" }
-  e.merge!({
-    :BOLD      => "\e[1m",
-    :UNDERLINE => "\e[4m",
-    :REVERSE   => "\e[7m",
-    :BLACK     => "\e[30m",
-    :RED       => "\e[31m",
-    :GREEN     => "\e[32m",
-    :YELLOW    => "\e[33m",
-    :BLUE      => "\e[34m",
-    :MAGENTA   => "\e[35m",
-    :CYAN      => "\e[36m",
-    :WHITE     => "\e[37m",
-    :RESET     => "\e[m",
+  seq = proc {|n| $stdout.isatty ? "\e[#{n}m" : "" }
 
-    :CLS       => "\e[2J",
-    :HOME      => "\e[H",
-#    :move      => proc {|l,c| "\e[#{l};#{c}H" },
-#    :save      => proc { "\e[s" },
-#    :undo      => proc { "\e[u" },
-  }) if $stdout.isatty
+  color = proc do |s|
+    n = {
+      :BLACK    => 30,
+      :RED      => 31,
+      :GREEN    => 32,
+      :YELLOW   => 33,
+      :BLUE     => 34,
+      :MAGENTA  => 35,
+      :CYAN     => 36,
+      :WHITE    => 37,
+    }[version_color]
 
-  color = proc {|s| e[:BOLD] + e[version_color] + s + e[:RESET] }
+    "#{seq[n]}#{s}#{seq[0]}"
+  end
+
+  cls  = "\e[2J"
+  move = proc {|y| "\e[#{y}H" }
 
   #
   # strings
   #
-  banner = <<-BANNER
-
+  banner1 = <<-'BANNER1'
       ######  #     # ######  #     #
       #     # #     # #     #  #   #
       #     # #     # #     #   # #
@@ -48,33 +47,47 @@ instance_eval do
       #   #   #     # #     #    #
       #    #  #     # #     #    #
       #     #  #####  ######     #
+  BANNER1
 
-  BANNER
+  banner2 =<<-'BANNER2'
+    ____          __           
+   |  _ \\ __  _ | ||_   __  __
+   | |_) || ||| || '_ \\| ||| ||
+   |  _ <<| ||| || |_) || |_| ||
+   |_||\_\\\__,//|_.__// \__, ||
+                         /___//
+  BANNER2
+
+  banner =  case banner_type
+              when :FIGLET  ; banner2
+              else          ; banner1
+            end
 
   descr = <<-DESCR
-
-  #{RUBY_DESCRIPTION.sub(/^ruby \S+/){ color[$&] }}
+  #{RUBY_DESCRIPTION.sub(/\Aruby \S+/){ color[$&] }}
   #{RUBY_COPYRIGHT}
-
   DESCR
 
   #
   # print banner
   #
-  unless $stdout.isatty
-    puts banner + descr
-  else
-    height = banner.split($/).length
-    blanks = ([$/] * height).join
+  if $stdout.isatty
+    lines = banner.split $/
+    top = 2
 
-    (0..height).each do |i|
-      puts e[:CLS] + e[:HOME]
-      puts color[ (blanks + banner).split($/)[i, height].join($/) ]
-      puts descr
+    [*0...lines.size].reverse.each_with_index do |y, i|
+      puts cls
+      puts move[top + y             ] + color[lines[0..i] * $/]
+      puts move[top + lines.size + 1] + descr
+      puts
 
       sleep 0.1
     end
+
+  else
+    puts $/ + banner + $/ + descr + $/
+
   end
 end
 
-# vim:set ts=2 sw=2 et fenc=UTF-8:
+# vim:set ts=2 sw=2 et:
